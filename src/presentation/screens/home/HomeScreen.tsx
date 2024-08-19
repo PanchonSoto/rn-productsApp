6b255a5743775385694215d9c58095d48e41de4f
Francisco Soto <panchon.ues@gmail.com>
2024-08-19 16:47:02 -0700

    first commit

---
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -0,0 +1,14 @@
+import { Button, Icon, Layout, Text } from '@ui-kitten/components';
+
+
+export const HomeScreen = () => {
+  return (
+    <Layout>
+      <Text>HomeScreen</Text>
+      <Icon name='facebook'/>
+      <Button>
+        Cerrar
+      </Button>
+    </Layout>
+  );
+}
